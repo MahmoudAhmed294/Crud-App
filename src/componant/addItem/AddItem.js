ec1d0b65fa6e69a10fc34f3f10d122b9ca2aff6b
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-10-10 23:29:31 -0400

    Modify function structures

--- src/componant/addItem/AddItem.js
+++ src/componant/addItem/AddItem.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React from 'react'
 
-const AddItem = ({ updataCourse, handleSubmit, current }) => {
+function AddItem ({ updataCourse, handleSubmit, current }) {
   return (
     <form onSubmit={handleSubmit} className='input-group mb-3'>
       <input
-        type="text"
-        className="form-control"
-        aria-describedby="basic-addon1"
-        placeholder="Please Enter Courser"
+        type='text'
+        className='form-control'
+        aria-describedby='basic-addon1'
+        placeholder='Please enter new course'
         value={current}
         onChange={updataCourse}
       />
       <div className='input-group-prepend'>
 
-      <button className="btn btn-success">Add Course</button>
+      <button className='btn btn-success'>Add Course</button>
       </div>
     </form>
-  );
-};
+  )
+}
+
 export default AddItem;
